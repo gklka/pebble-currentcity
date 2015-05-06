2fb570947ab44fcae8dd9c7a36ff43d34e422e91
Gruber Kristóf <gk@lka.hu>
2015-05-06 23:02:46 +0200

    Added watch face and text label for city

--- src/app.js
+++ src/app.js
@@ -5,52 +5,31 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+var main = new UI.Window({
+	backgroundColor: 'white',
+	fullscreen: true,
+	scrollable: false
 });
+
+var timeText = new UI.TimeText({
+	text: "%H:%M",
+	color: 'black',
+    font: 'bitham-42-bold',
+	textAlign: 'center',
+	backgroundColor: 'clear',
+	position: new Vector2(0, 30),
+	size: new Vector2(144, 50)
+});
+
+var locationText = new UI.Text({
+	text: "City",
+	color: 'black',
+	font: 'gothic-24-bold',
+	position: new Vector2(0, 80),
+	size: new Vector2(144, 50)	
+});
+
+main.add(timeText);
+main.add(locationText);
 
 main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
